cd915ff2402e805f556f786fa89fcffbe2566f3e
Mattia Rombi <mattyro96@gmail.com>
2023-07-02 00:36:08 +0200

    Added comments to explain code

--- control.lua
+++ control.lua
@@ -22,7 +22,7 @@ for i = 1, #constants.player_events do
 	script.on_event(
 		constants.player_events[i],
 		function(e)
-			player_data.fix_all(game.players[e.player_index], true)
+			player_data.fix_all(game.players[ e.player_index --[[@as uint]] ], true)
 		end
 	)
 end
@@ -31,19 +31,26 @@ for i = 1, #constants.gui_events do
 	script.on_event(
 		constants.gui_events[i],
 		function(e)
-			local player = game.players[e.player_index]
+			local player = game.players[ e.player_index --[[@as uint]] ]
 
+			-- Add the tick functions to perform manual-inventory-sort fixes
 			global_data.tick_functions.manual_inventory_sort = function()
+				-- Fix the manual-inventory-sort mod
 				player_data.fix_manual_inventory_sort(player)
+				-- Remove the tick function from the global_data
 				global_data.tick_functions.manual_inventory_sort = nil
 			end
 
+			-- Add the tick functions to perform task-list fixes
 			global_data.tick_functions.task_list = function()
+				-- Fix the task-list mod
 				player_data.fix_task_list(player)
+				-- Remove the tick function from the global_data
 				global_data.tick_functions.task_list = nil
 			end
 
-			player_data.toggle_task_list(player, e.element)
+			-- Handle task-list button click event (if related element exists)
+			player_data.toggle_task_list(player, e.element --[[@as LuaGuiElement?]])
 		end
 	)
 end
@@ -51,6 +58,7 @@ end
 script.on_event(
 	defines.events.on_string_translated,
 	function(e)
+		-- Apply the tooltip translation to manual-inventory-sort buttons
 		player_data.translate_manual_inventory_sort(game.players[e.player_index], e.id, e.result)
 	end
 )
--- scripts/player-data.lua
+++ scripts/player-data.lua
@@ -2,13 +2,17 @@ local mod_gui = require("__core__.lualib.mod-gui")
 
 local player_data = {}
 
+--- @param player LuaPlayer
 local has_inventory_opened = function(player)
+    -- Check if the player has opened itself, the blueprint library or another player
     if player.opened_self or (player.opened_gui_type == defines.gui_type.blueprint_library) or player.opened_gui_type == (defines.gui_type.other_player) then
         return true
     end
 
+    -- Check if the player has opened an entity
     if player.opened_gui_type == defines.gui_type.entity then
         for _, index in pairs(defines.inventory) do
+            -- Check if the entity has an inventory and if it is valid
             local inventory = player.opened.get_inventory(index)
             if inventory and inventory.valid then
                 return true
@@ -17,34 +21,45 @@ local has_inventory_opened = function(player)
     end
 end
 
+--- @param player LuaPlayer
+--- @param force? boolean
 player_data.fix_manual_inventory_sort = function(player, force)
+    -- Check if the mod is enabled and the player has the setting enabled
     if not game.active_mods["manual-inventory-sort"] or not settings.player["manual-inventory-sort-buttons"] then
         return
     end
 
+    -- Check if buttons has been created for the player
     if player.gui.left["manual-inventory-sort-buttons"] then
+        -- Perform a better check on inventory
         if has_inventory_opened(player) then
             local localised_strings = {
                 { "gui.character" },
                 { "gui-logistic.trash-slots" }
             }
 
+            -- Check if the player has opened a supported entity by manual-inventory-sort
             for _, item in ipairs(player.gui.left["manual-inventory-sort-buttons"].children_names) do
                 if item == "manual-inventory-sort-opened" then
                     localised_strings[3] = player.opened.localised_name
                 end
             end
 
-            local request_ids = player.request_translations(localised_strings)
+            -- Try to request the localised strings from the player translations
+            local request_ids = player.request_translations(localised_strings) or localised_strings
 
+            -- Create the new buttons for all types of relative GUIs
             for _, relative_gui_type in pairs(defines.relative_gui_type) do
                 local name = "manual-inventory-sort-buttons-" .. relative_gui_type
                 local anchor = { gui = relative_gui_type, position = defines.relative_gui_position.left }
 
+                -- Destroy the old frame buttons
                 if player.gui.relative[name] then
                     player.gui.relative[name].destroy()
                 end
 
+                --- @type LuaGuiElement
+                -- Create the new frame buttons
                 local frame = player.gui.relative.add({
                     name = name,
                     type = "frame",
@@ -53,6 +68,7 @@ player_data.fix_manual_inventory_sort = function(player, force)
                     anchor = anchor
                 })
 
+                -- Add player inventory sort button
                 frame.add({
                     type = "sprite-button",
                     name = "manual-inventory-sort-player",
@@ -62,6 +78,7 @@ player_data.fix_manual_inventory_sort = function(player, force)
                 })
 
                 if player.opened_self and player.force.character_logistic_requests then
+                    -- Add player trash sort button
                     frame.add({
                         type = "sprite-button",
                         name = "manual-inventory-sort-player-trash",
@@ -71,6 +88,7 @@ player_data.fix_manual_inventory_sort = function(player, force)
                     })
                 end
 
+                -- Add opened entity sort button
                 if localised_strings[3] then
                     frame.add({
                         type = "sprite-button",
@@ -83,28 +101,34 @@ player_data.fix_manual_inventory_sort = function(player, force)
             end
         end
 
+        -- Destroy the buttons created by manual-inventory-sort
         player.gui.left["manual-inventory-sort-buttons"].destroy()
     end
-
-    -- player.print(serpent.block("fix_manual_inventory_sort"))
 end
 
+--- @param player LuaPlayer
+--- @param id uint
+--- @param result string
 player_data.translate_manual_inventory_sort = function(player, id, result)
+    -- Update buttons tooltip for all types of relative GUIs
     for _, relative_gui_type in pairs(defines.relative_gui_type) do
         local name = "manual-inventory-sort-buttons-" .. relative_gui_type
 
+        -- Check if the buttons has been created
         if player.gui.relative[name] then
             for _, item in ipairs(player.gui.relative[name].children) do
+                -- Find the button with the matching tooltip id
                 if item.tooltip == tostring(id) then
+                    -- Update the tooltip with the new translation
                     item.tooltip = "Sort " .. string.lower(result)
                 end
             end
         end
-
-        -- player.print(serpent.block("translate_manual_inventory_sort"))
     end
 end
 
+--- @param player LuaPlayer
+--- @param force? boolean
 player_data.fix_task_list = function(player, force)
     if not game.active_mods["TaskList"] then
         return
@@ -112,11 +136,14 @@ player_data.fix_task_list = function(player, force)
 
     local button_flow = mod_gui.get_button_flow(player)
 
+    -- Destroy the task-list button if it exists and force is true
     if force and button_flow.task_maximize_button then
         button_flow.task_maximize_button.destroy()
     end
 
+    -- Check if task-list button already exists
     if not button_flow.task_maximize_button then
+        -- Create the task-list button
         button_flow.add({
             name = "task_maximize_button",
             tooltip = "Task List",
@@ -125,16 +152,19 @@ player_data.fix_task_list = function(player, force)
             sprite = "matrixdj96_task_list_icon"
         })
     end
-
-    -- player.print(serpent.block("fix_task_list"))
 end
 
+--- @param player LuaPlayer
+--- @param element? LuaGuiElement
 player_data.toggle_task_list = function(player, element)
+    -- Check if the task-list button was clicked and toggle the task-list window
     if element and element.name == "task_maximize_button" and player.gui.screen.tlst_tasks_window then
         player.gui.screen.tlst_tasks_window.visible = not player.gui.screen.tlst_tasks_window.visible
     end
 end
 
+--- @param player LuaPlayer
+--- @param force boolean
 player_data.fix_all = function(player, force)
     player_data.fix_manual_inventory_sort(player, force)
     player_data.fix_task_list(player, force)
