ad1ca45a338ded9b56ae4e0fbacd0c0cc6252004
Mattia Rombi <mattyro96@gmail.com>
2023-07-02 02:11:58 +0200

    Removed heavy gui event hooks

--- constants.lua
+++ constants.lua
@@ -1,18 +1,8 @@
 local constants = {}
 
 constants.gui_events = {
-	defines.events.on_gui_checked_state_changed,
 	defines.events.on_gui_click,
-	defines.events.on_gui_closed,
-	defines.events.on_gui_confirmed,
-	defines.events.on_gui_elem_changed,
-	defines.events.on_gui_location_changed,
-	defines.events.on_gui_opened,
-	defines.events.on_gui_selected_tab_changed,
-	defines.events.on_gui_selection_state_changed,
-	defines.events.on_gui_switch_state_changed,
-	defines.events.on_gui_text_changed,
-	defines.events.on_gui_value_changed
+	defines.events.on_gui_opened
 }
 
 constants.player_events = {
