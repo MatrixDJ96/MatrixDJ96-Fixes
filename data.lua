23833a7cd542a26dc2e02a82ba3adbc0b72eef80
Mattia Rombi <mattyro96@gmail.com>
2023-07-03 14:42:56 +0200

    Improved data stage sprite creation

--- data.lua
+++ data.lua
@@ -1,20 +1,28 @@
-data:extend(
-	{
-		{
-			type = "sprite",
-			name = "matrixdj96_task_list_icon",
-			filename = "__MatrixDJ96-Fixes__/graphics/icons/task_list.png",
-			flags = { "linear-minification", "linear-magnification" },
-			width = 64,
-			height = 64
-		},
-		{
-			type = "sprite",
-			name = "matrixdj96_trash_icon",
-			filename = "__MatrixDJ96-Fixes__/graphics/icons/trash.png",
-			flags = { "linear-minification", "linear-magnification" },
-			width = 64,
-			height = 64
-		}
+---@param name string
+---@param path string
+---@return table
+local function create_sprite(name, path, prefix)
+	-- Check if prefix is nil
+	if prefix == nil then
+		-- Set default prefix to mod name
+		prefix = "__MatrixDJ96-Fixes__"
+	end
+
+	-- Create sprite definition
+	return {
+		type = "sprite",
+		name = "matrixdj96_" .. name,
+		filename = prefix .. "/graphics/" .. path .. ".png",
+		flags = { "linear-minification", "linear-magnification" },
+		width = 64,
+		height = 64
 	}
-)
+end
+
+-- Generate custom sprites
+local task_list_icon = create_sprite("task_list_icon", "icons/task_list")
+local trash_icon = create_sprite("trash_icon", "icons/trash")
+
+-- Add custom sprites to data
+data:extend({ task_list_icon, trash_icon })
+
